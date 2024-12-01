51e03e5b07097e1fceb7331a5cfd2cdc8d91504f
obeida.slimani@loreal.com <oslimani-oa>
2024-12-01 17:54:55 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
-    # Usernames must start with a letter
+    # Usernames must be alpha
     if username[0].isalpha ():
         return False
     return True
